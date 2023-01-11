263cab55decc6f45fc905fa015702064dd8772af
JoleneDwwm <jolene.dwwm2022@gmail.com>
2023-01-11 15:31:41 +0100

    adding style
    may be reorganized in a CSS file later

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
-//Setup
+try
+{
+    $mysqlClient = new PDO('mysql:host=localhost;dbname=brief5;charset=utf8', 'root', '' );
+}
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage("Il y a une erreur!" ));
+}
+
+//Template setup pt1/4
 require_once('template.class.php');
 define('TEMPLATES_PATH', 'templates');
-
-//Instanciate new object
+//Instanciate new object 2/4
 $template = new Template(TEMPLATES_PATH.'/tpl.html'); 
-
-//Assign values
-$template->assign('title', 'Hello');
-$template->assign('text', 'World');
-
-//Show content
+//Assign values 3/4
+$template->assign('title', 'Bonjour');
+$template->assign('text', 'Monde');
+//Show content 4/4 
 $template->show();
 ?>
