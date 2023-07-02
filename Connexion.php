c7734e7f6e0b98e0861d021121f115677cf9addb
Jolene <116793905+JoleneDwwm@users.noreply.github.com>
2023-07-02 22:55:05 +0200

    Update Connexion.php
    Brute-forced a reversal of previous changes

--- Connexion.php
+++ Connexion.php
@@ -4,11 +4,7 @@
 
 try
 {
-<<<<<<< HEAD
-    $mysqlClient = new PDO('mysql:host=aws.connect.psdb.cloud;dbname=brief5;charset=utf8', 'sal9oejw5lvfm8j9prto', 'pscale_pw_AlAuQlxPIeTcX8lMsiAmhObAV8A2fThAO2j1qnnztF2' );
-=======
     $mysqlClient = new PDO('mysql:host=localhost;dbname=brief5;charset=utf8', 'root', '' );
->>>>>>> parent of e3f61b9 (Test Netlify Planetscale)
 }
 catch(Exception $e)
 {
